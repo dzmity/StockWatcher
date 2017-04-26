4539e6d97179ef045fbb9f708487a57420b99cdb
Dzmitry Rafalovich <dzmitry_rafalovich@epam.com>
2017-04-26 13:40:38 +0300

    Generate sample mvn gwt application using webAppCreator.

---
+++ src/main/java/by/epam/rafalovich/gwt/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package by.epam.rafalovich.gwt.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+  void greetServer(String input, AsyncCallback<String> callback)
+      throws IllegalArgumentException;
+}
